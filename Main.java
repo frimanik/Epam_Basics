1b965630ad0be7c858d249be371fbf413e55c704
frimanik <52167218+frimanik@users.noreply.github.com>
2019-07-09 22:23:00 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -23,17 +23,15 @@ public class Main {
     //Array3 - fix34
     public int[] fix34(int[] nums) {
         int temp = 0;
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++)
             if (nums[i] == 3) {
-                for (int j = 0; j < nums.length; j++) {
+                for (int j = 0; j < nums.length; j++)
                     if (nums[j] == 4 && nums[j - 1] != 3) {
                         temp = nums[i + 1];
                         nums[i + 1] = nums[j];
                         nums[j] = temp;
                     }
-                }
             }
-        }
         return nums;
     }
 
